857f85b9240084742b66c67ff585c8bc58b7b8c8
simon <simon@myelintek.local>
2024-05-13 18:37:32 +0000

    remove mlflow set experiment_name

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def process_bentoml(model_path):
 def train(hyp, opt, device, tb_writer=None):
     mlflow_signature = None
     mlflow_experience_name = "yolov7"
-    mlflow.set_experiment(mlflow_experience_name)
+    #mlflow.set_experiment(mlflow_experience_name)
     logger.info(colorstr('hyperparameters: ') + ', '.join(f'{k}={v}' for k, v in hyp.items()))
     save_dir, epochs, batch_size, total_batch_size, weights, rank, freeze = \
         Path(opt.save_dir), opt.epochs, opt.batch_size, opt.total_batch_size, opt.weights, opt.global_rank, opt.freeze
